0a096c0a619206b28c273bd96c7bbb7a1baf7efd
RichardKhanhWin <nguyenrichard023@gmail.com>
2024-04-11 17:45:49 -0500

    Use ToDoItem

--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -1,9 +1,11 @@
-export default function ToDoItem({title, description}: {title: string; description: string;}) {
+export default function ToDoItem({ title, description, done }: { title: string, description: string, done: boolean }) {
 	return (
-		<>
-			<h2>{title}</h2>
+		<div className="bg-slate-200 text-black p-3 w-96 my-4">
+			<div className="flex justify-between">
+				<h2>{title}</h2>
+				<input type="checkbox" name="complete" defaultChecked={done}/>
+			</div>
 			<p>{description}</p>
-			<input type="checkbox" name="complete"/>
-		</>
+		</div>
 	);
 }
