bae990de081eac0380ded0a932516f8854659da9
RichardKhanhWin <nguyenrichard023@gmail.com>
2024-04-11 21:08:05 -0500

    Add Create functionality to toDoItem

--- src/pages/api/items/index.ts
+++ src/pages/api/items/index.ts
@@ -1,12 +1,25 @@
 import prisma from "@/lib/prisma";
 
 export default async function handle(req, res) {
-	// Get to-do items from database
-	try {
-		const toDoItems = await prisma.toDoItem.findMany();
-		res.status(200).json(toDoItems)
-	} catch (err) {
-		console.log(err);
-		res.status(400).json({ error: "An error occured."});
+
+	if (req.method === "GET") {
+		// Get to-do items from database
+		try {
+			const toDoItems = await prisma.toDoItem.findMany();
+			res.status(200).json(toDoItems)
+		} catch (err) {
+			console.log(err);
+			res.status(400).json({ error: "An error occured."});
+		}
+	} else if (req.method === "POST") {
+		const { title, description } = req.body;
+		const result = await prisma.toDoItem.create({
+			data: {
+				title,
+				description
+			}
+		});
+
+		res.json(result);
 	}
 }
--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -2,24 +2,42 @@
 
 import React, { useState } from "react";
 import "@/app/globals.css";
+import Router from "next/router";
 
 export default function CreatePage() {
 	const [title, setTitle] = useState("");
 	const [description, setDescription] = useState("");
 
-	const onSubmit = (e) => {
+	const onSubmit = async (e) => {
 		e.preventDefault();
+
+		try {
+			const body = { title, description };
+			await fetch("/api/items", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(body)
+			});
+
+			await Router.push("/");
+		} catch (err) {
+			console.log(err);
+		}
 	}
 
 	return (
-		<form>
-			<label id="title">Title</label>
-			<input name="title" />
+		<form className="flex flex-col items-center justify-between">
+			<div className="flex flex-col">
+				<label id="title">Title</label>
+				<input className="text-black" name="title" onChange={(e) => setTitle(e.target.value)} value={title} />
+			</div>
 
-			<label id="description">Description</label>
-			<textarea name="description"/>
+			<div className="flex flex-col">
+				<label id="description">Description</label>
+				<textarea className="text-black" name="description" onChange={(e) => setDescription(e.target.value)} value={description} />
+			</div>
 
-			<button onClick={onSubmit}>Create</button>
+			<button onClick={onSubmit} value="Create">Create</button>
 		</form>
 	)
 }
