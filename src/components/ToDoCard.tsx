c533adcb1f23ed587364dac905d5eff33ae2bd74
RichardKhanhWin <nguyenrichard023@gmail.com>
2024-06-10 15:05:58 -0500

    Use server actions instead of API routes

--- src/components/ToDoCard.tsx
+++ src/components/ToDoCard.tsx
@@ -1,36 +1,35 @@
-export default function ToDoCard({ item_id, title, description, done }: { item_id: number, title: string, description: string, done: boolean }) {
+'use client';
+
+import { useState } from 'react';
+import { toggleDone, deleteToDoItem } from '@/lib/actions';
+import { CheckIcon, PencilIcon, TrashIcon } from '@heroicons/react/24/solid';
+import Link from 'next/link';
+
+export default function ToDoCard({ item_id, title, description, done }: { item_id: number, title: string, description: string | null, done: boolean }) {
+	const toggle = toggleDone.bind(null, item_id);
+
+	const deleteItem = deleteToDoItem.bind(null, item_id);
+
+	const [complete, setComplete] = useState(done);
+
 	return (
 		<div className="bg-slate-200 text-black border-2 border-slate-800 p-3 w-96 my-4">
 			<div className="flex justify-between">
 				<h2 className="font-bold">{title}</h2>
-				<div className="flex justify-between gap-x-2">
-					<input type="checkbox" name="complete" defaultChecked={done}
-						onChange={(e) => {
-							fetch("api/items", {
-								method: "PUT",
-								headers: {
-									"Content-Type": "application/json"
-								},
-								body: JSON.stringify({
-									id: item_id,
-									title,
-									description,
-									done: e.target.checked
-								})
-							})
-						}} />
-
-						<button onClick={(e) => fetch("api/items", {
-							method: "DELETE",
-							headers: {
-								"Content-Type": "application/json"
-							},
-							body: JSON.stringify({
-								id: item_id
-							})
-						})}>
-							X
+				<div className="flex justify-between items-center gap-x-2">
+					<form action={toggle}>
+						<button onClick={() => { setComplete(!complete) }} className="bg-white w-4 h-4 border-2 border-slate-400 block">
+							{ complete && <CheckIcon /> }
 						</button>
+					</form>
+					<Link href={`/edit/${item_id}`} className="bg-white w-4 h-4 border-2 border-slate-400 block">
+						<PencilIcon />
+					</Link>
+					<form action={deleteItem}>
+						<button className="bg-white w-4 h-4 border-2 border-slate-400 block">
+							<TrashIcon />
+						</button>
+					</form>
 				</div>
 			</div>
 			<p>{description}</p>
