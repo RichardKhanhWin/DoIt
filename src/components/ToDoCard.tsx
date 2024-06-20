2b04e2f72e85e9ab7eb8bd0d12f17dcfb636a30b
RichardKhanhWin <nguyenrichard023@gmail.com>
2024-06-19 22:49:40 -0500

    Replace checkbox button element with Checkbox component

--- src/components/ToDoCard.tsx
+++ src/components/ToDoCard.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { toggleDone, deleteToDoItem } from '@/lib/actions';
 import { CheckIcon, PencilIcon, TrashIcon } from '@heroicons/react/24/solid';
 import Link from 'next/link';
+import Checkbox from './Checkbox';
 
 export default function ToDoCard({ item_id, title, description, done }: { item_id: number, title: string, description: string | null, done: boolean }) {
 	const toggle = toggleDone.bind(null, item_id);
@@ -22,9 +23,7 @@ export default function ToDoCard({ item_id, title, description, done }: { item_i
 			<div className="flex justify-between">
 				<h2 className="font-bold">{title}</h2>
 				<div className="flex justify-between items-center gap-x-2">
-					<button onClick={handleClick} className="bg-white w-4 h-4 border-2 border-slate-400 block">
-						{ complete && <CheckIcon /> }
-					</button>
+					<Checkbox onClick={handleClick} defaultChecked={complete} className="bg-white w-4 h-4 border-2 border-slate-400 block" />
 					<Link href={`/edit/${item_id}`} className="bg-white w-4 h-4 border-2 border-slate-400 block">
 						<PencilIcon />
 					</Link>
