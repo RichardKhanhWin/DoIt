57e7ececdea870cf73dbcd96b13e26301cbc41fd
RichardKhanhWin <nguyenrichard023@gmail.com>
2024-06-19 23:16:39 -0500

    Update

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,18 +1,19 @@
 import { createToDoItem } from "@/lib/actions";
+import Checkbox from "@/components/Checkbox";
 
 export default function CreateToDoPage() {
 	return (
-		<form action={createToDoItem} className="flex justify-center">
-			<div className="flex-col justify-between">
-				<div>
-					<input name="title" defaultValue={""} className="dark:bg-slate-700 p-1"/>
-					<input type="checkbox" name="done" defaultChecked={false} />
+		<form action={createToDoItem} className="flex items-center justify-center w-1/2 m-auto h-screen">
+			<div className="flex grow items-center flex-col gap-y-[8px]">
+				<div className="flex min-w-full justify-between gap-x-[4px]">
+					<input name="title" defaultValue={""} className="dark:bg-slate-700 dark:text-white p-1 grow"/>
+					<Checkbox name="done" defaultChecked={false} className="h-8 w-8 bg-slate-200 border-black border-2 text-black" />
+				</div>
+				<div className="flex min-w-full">
+					<textarea name="description" rows={10} className="dark:bg-slate-700 dark:text-white p-1 grow resize-none" />
 				</div>
 				<div>
-					<textarea name="description" defaultValue={""} className="dark:bg-slate-700 p-1" />
-				</div>
-				<div>
-					<button type="submit">Create</button>
+					<button type="submit" className="p-2 rounded-md dark:bg-indigo-900 dark:text-white">Create</button>
 				</div>
 			</div>
 		</form>
