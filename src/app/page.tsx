0da075b07c304f527365a324e7189774de487373
RichardKhanhWin <nguyenrichard023@gmail.com>
2024-06-22 16:43:10 -0500

    Update UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   const data = await fetchToDoItems();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-2">
+    <main className="flex min-h-screen flex-col items-center p-2">
       <div className="flex-col">
         { data.map((element: ToDoItem, index: number) => {
             return (
